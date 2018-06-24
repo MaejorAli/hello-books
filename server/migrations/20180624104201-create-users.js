50154a0d43fb8ca88cffd4b1767d6d2c7e8b0ae7
Ali Shaibu <alishaibu2002@gmail.com>
2018-06-24 12:02:47 +0100

    created user model and migration files

---
+++ server/migrations/20180624104201-create-users.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    firstname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    membershiplevel: {
+      type: Sequelize.ENUM,
+      defaultValue: 'Silver',
+      values: ['Silver', 'Gold', 'Platinum'],
+    },
+    role: {
+      type: Sequelize.ENUM,
+      defaultValue: 'User',
+      values: ['ChiefAdmin', 'User', 'Admin'],
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Users'),
+};
---
+++ server/models/users.js
@@ -0,0 +1,47 @@
+export default (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    id: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    membershiplevel: {
+      type: DataTypes.ENUM,
+      defaultValue: 'Silver',
+      values: ['Silver', 'Gold', 'Platinum'],
+    },
+    role: {
+      type: DataTypes.ENUM,
+      defaultValue: 'User',
+      values: ['ChiefAdmin', 'Admin', 'User'],
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    classMethods: {
+      associate: () => {
+        // associations can be defined here
+      },
+    },
+  });
+  Users.associate = (models) => {
+    Users.hasMany(models.Books, { as: 'books', foreignKey: 'user' });
+  };
+  return Users;
+};
